cf610d2bc9bae69f1b36bfea8325a8d9eb4d992f
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-06 10:52:04 +0200

    Added needed dependencies for messaging

--- src/main/java/com/messaging/stompmessaging/controller/GreetingController.java
+++ src/main/java/com/messaging/stompmessaging/controller/GreetingController.java
@@ -2,13 +2,19 @@ package com.messaging.stompmessaging.controller;
 
 import com.messaging.stompmessaging.model.Greeting;
 import com.messaging.stompmessaging.model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+@Controller
 public class GreetingController {
 
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
     public Greeting greeting(Message message) throws  Exception{
         Thread.sleep(1000);
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
+
 
 }
