0076d20f4178f5df209dc1f664750875376e624a
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-06 21:05:21 +0200

    back to original structure

--- src/main/java/com/messaging/stompmessaging/controller/GreetingController.java
+++ src/main/java/com/messaging/stompmessaging/controller/GreetingController.java
@@ -5,6 +5,9 @@ import com.messaging.stompmessaging.model.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.util.HtmlUtils;
 
 @Controller
--- src/main/java/com/messaging/stompmessaging/model/Message.java
+++ src/main/java/com/messaging/stompmessaging/model/Message.java
@@ -8,6 +8,10 @@ public class Message {
 
     }
 
+    public Message(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
