ddb090cc939f0189f96f1b1403acfd70ef8de9c7
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-05-06 10:32:56 +0200

    Created a Plain old java object for carrying message

---
+++ src/main/java/com/messaging/stompmessaging/model/Message.java
@@ -0,0 +1,18 @@
+package com.messaging.stompmessaging.model;
+
+public class Message {
+
+    public String name;
+
+    public Message() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
